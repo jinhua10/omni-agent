685a1a76885f62cd6ccd204c2aba3c9e275332c8
Jinhua Yu <1015770492@qq.com>
2025-12-28 14:43:05 +0800

    利用我们的架构，做一个类似于Copilot的知识库问答系统

--- omni-agent-document-processor-starter/src/main/java/top/yumbo/ai/omni/document/processor/starter/processor/PPTProcessor.java
+++ omni-agent-document-processor-starter/src/main/java/top/yumbo/ai/omni/document/processor/starter/processor/PPTProcessor.java
@@ -1,5 +1,6 @@
 package top.yumbo.ai.omni.document.processor.starter.processor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.xslf.usermodel.*;
 import org.apache.poi.hslf.usermodel.*;
 import top.yumbo.ai.omni.document.processor.*;
